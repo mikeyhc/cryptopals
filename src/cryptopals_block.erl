7a166563864281749c58a5705b22938475c2d440
mikeyhc <mikeyhc@atmosia.net>
2021-10-05 16:37:21 -0400

    started work on aes ecb

--- src/cryptopals_block.erl
+++ src/cryptopals_block.erl
@@ -1,9 +1,123 @@
 -module(cryptopals_block).
 
--export([pkcs7_padding/2]).
+-export([pkcs7_padding/2, aes/4]).
+-export_type([aes_mode/0]).
+
+-ifdef(TEST).
+-compile(export_all).
+-endif.
+
+-type aes_mode() :: aes_128_ecb.
+-type aes_operation() :: encrypt | decrypt.
+
+%% public api
 
 -spec pkcs7_padding(binary(), pos_integer()) -> binary().
 pkcs7_padding(Message, BlockSize) ->
     PadLen = BlockSize - size(Message) rem BlockSize,
     Pad = list_to_binary(lists:duplicate(PadLen, PadLen)),
     <<Message/binary, Pad/binary>>.
+
+-spec aes(aes_mode(), aes_operation(), binary(), binary()) -> binary().
+aes(aes_128_ecb, encrypt, Key, PlainText) ->
+    aes_ecb_encrypt(16, Key, PlainText).
+
+%% internal functions
+
+forward_sbox(Byte) ->
+    SBox = {{16#63, 16#7c, 16#77, 16#7b, 16#f2, 16#6b, 16#6f, 16#c5,
+             16#30, 16#01, 16#67, 16#2b, 16#fe, 16#d7, 16#ab, 16#76},
+            {16#ca, 16#82, 16#c9, 16#7d, 16#fa, 16#59, 16#47, 16#f0,
+             16#ad, 16#d4, 16#a2, 16#af, 16#9c, 16#a4, 16#72, 16#c0},
+            {16#b7, 16#fd, 16#93, 16#26, 16#36, 16#3f, 16#f7, 16#cc,
+             16#34, 16#a5, 16#e5, 16#f1, 16#71, 16#d8, 16#31, 16#15},
+            {16#04, 16#c7, 16#23, 16#c3, 16#18, 16#96, 16#05, 16#9a,
+             16#07, 16#12, 16#80, 16#e2, 16#eb, 16#27, 16#b2, 16#75},
+            {16#09, 16#83, 16#2c, 16#1a, 16#1b, 16#6e, 16#5a, 16#a0,
+             16#52, 16#3b, 16#d6, 16#b3, 16#29, 16#e3, 16#2f, 16#84},
+            {16#53, 16#d1, 16#00, 16#ed, 16#20, 16#fc, 16#b1, 16#5b,
+             16#6a, 16#cb, 16#be, 16#39, 16#4a, 16#4c, 16#58, 16#cf},
+            {16#d0, 16#ef, 16#aa, 16#fb, 16#43, 16#4d, 16#33, 16#85,
+             16#45, 16#f9, 16#02, 16#7f, 16#50, 16#3c, 16#9f, 16#a8},
+            {16#51, 16#a3, 16#40, 16#8f, 16#92, 16#9d, 16#38, 16#f5,
+             16#bc, 16#b6, 16#da, 16#21, 16#10, 16#ff, 16#f3, 16#d2},
+            {16#cd, 16#0c, 16#13, 16#ec, 16#5f, 16#97, 16#44, 16#17,
+             16#c4, 16#a7, 16#7e, 16#3d, 16#64, 16#5d, 16#19, 16#73},
+            {16#60, 16#81, 16#4f, 16#dc, 16#22, 16#2a, 16#90, 16#88,
+             16#46, 16#ee, 16#b8, 16#14, 16#de, 16#5e, 16#0b, 16#db},
+            {16#e0, 16#32, 16#3a, 16#0a, 16#49, 16#06, 16#24, 16#5c,
+             16#c2, 16#d3, 16#ac, 16#62, 16#91, 16#95, 16#e4, 16#79},
+            {16#e7, 16#c8, 16#37, 16#6d, 16#8d, 16#d5, 16#4e, 16#a9,
+             16#6c, 16#56, 16#f4, 16#ea, 16#65, 16#7a, 16#ae, 16#08},
+            {16#ba, 16#78, 16#25, 16#2e, 16#1c, 16#a6, 16#b4, 16#c6,
+             16#e8, 16#dd, 16#74, 16#1f, 16#4b, 16#bd, 16#8b, 16#8a},
+            {16#70, 16#3e, 16#b5, 16#66, 16#48, 16#03, 16#f6, 16#0e,
+             16#61, 16#35, 16#57, 16#b9, 16#86, 16#c1, 16#1d, 16#9e},
+            {16#e1, 16#f8, 16#98, 16#11, 16#69, 16#d9, 16#8e, 16#94,
+             16#9b, 16#1e, 16#87, 16#e9, 16#ce, 16#55, 16#28, 16#df},
+            {16#8c, 16#a1, 16#89, 16#0d, 16#bf, 16#e6, 16#42, 16#68,
+             16#41, 16#99, 16#2d, 16#0f, 16#b0, 16#54, 16#bb, 16#16}},
+    A = Byte bsr 4 + 1,
+    B = Byte band 16#f + 1,
+    element(B, element(A, SBox)).
+
+aes_ecb_encrypt(KeySize, Key, _PlainText) when size(Key) =/= KeySize ->
+    error({invalid_key, Key, KeySize});
+aes_ecb_encrypt(KeySize, Key, PlainText) ->
+    MatrixSize = KeySize / 4,
+    KeyMatrix = block_matrix(MatrixSize, Key),
+    TextMatricies = to_matricies(MatrixSize, pkcs7_padding(PlainText, KeySize)),
+    FinalMatrix = run_aes(0, KeyMatrix, TextMatricies),
+    FinalMatrix.
+
+to_matricies(_Size, X) -> X.
+
+run_aes(_Idx, _Key, Text) -> Text.
+
+block_xor(A, B) ->
+    F = fun({X, Y}) -> X bxor Y end,
+    list_to_binary(lists:map(F, lists:zip(binary_to_list(A),
+                                          binary_to_list(B)))).
+
+block_matrix(Size, Text) ->
+    block_matrix(Size, Text, []).
+
+block_matrix(Size, Text, Acc) when size(Text) =:= Size ->
+    lists:reverse([Text|Acc]);
+block_matrix(Size, Text, Acc) ->
+    Head = binary:part(Text, 0, Size),
+    Tail = binary:part(Text, Size, size(Text) - Size),
+    block_matrix(Size, Tail, [Head|Acc]).
+
+aes_roundkey(Const, [W0, W1, W2, W3]) ->
+    ShiftW3 = circular_byte_left_shift(W3),
+    SubW3 = byte_substitution(ShiftW3),
+    ConstW3 = add_round_constant(Const, SubW3),
+    W4 = block_xor(W0, ConstW3),
+    W5 = block_xor(W1, W4),
+    W6 = block_xor(W2, W5),
+    W7 = block_xor(W3, W6),
+    [W4, W5, W6, W7].
+
+circular_byte_left_shift(<<A, B, C, D>>) ->
+    <<B, C, D, A>>.
+
+byte_substitution(<<A, B, C, D>>) ->
+    SA = forward_sbox(A),
+    SB = forward_sbox(B),
+    SC = forward_sbox(C),
+    SD = forward_sbox(D),
+    <<SA, SB, SC, SD>>.
+
+add_round_constant(Const, <<A, B, C, D>>) ->
+    CA = Const bxor A,
+    <<CA, B, C, D>>.
+
+round_constant(N) ->
+    Constants = {16#01, 16#02, 16#04, 16#08, 16#10,
+                 16#20, 16#40, 16#80, 16#1B, 16#36},
+    element(N, Constants).
+
+apply_aes(KeyMatrix, TextMatrix) ->
+    F = fun({X, Y}) -> block_xor(X, Y) end,
+    lists:map(F, lists:zip(KeyMatrix, TextMatrix)).
---
+++ test/aes_tests.erl
@@ -0,0 +1,61 @@
+-module(aes_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(SIMPLE_KEY, <<"Thats my Kung Fu">>).
+-define(SIMPLE_PLAINTEXT, <<"Two One Nine Two">>).
+
+block_matrix_test() ->
+    Expected = [<<16#54, 16#68, 16#61, 16#74>>,
+                <<16#73, 16#20, 16#6D, 16#79>>,
+                <<16#20, 16#4B, 16#75, 16#6E>>,
+                <<16#67, 16#20, 16#46, 16#75>>],
+    ?assertEqual(Expected, cryptopals_block:block_matrix(4, ?SIMPLE_KEY)).
+
+first_roundkey_test() ->
+    Expected = cryptopals_bytes:new_hex(<<"e232fcf191129188b159e4e6d679a293">>),
+    Matrix = cryptopals_block:block_matrix(4, ?SIMPLE_KEY),
+    Output = cryptopals_block:aes_roundkey(16#01, Matrix),
+    MatrixBinary = matrix_binary(Output),
+    ?assertEqual(Expected, cryptopals_bytes:hex_encode(MatrixBinary)).
+
+all_roundkey_test() ->
+    Expected = [<<"e232fcf191129188b159e4e6d679a293">>,
+                <<"56082007c71ab18f76435569a03af7fa">>,
+                <<"d2600de7157abc686339e901c3031efb">>,
+                <<"a11202c9b468bea1d75157a01452495b">>,
+                <<"b1293b3305418592d210d232c6429b69">>,
+                <<"bd3dc287b87c47156a6c9527ac2e0e4e">>,
+                <<"cc96ed1674eaaa031e863f24b2a8316a">>,
+                <<"8e51ef21fabb4522e43d7a0656954b6c">>,
+                <<"bfe2bf904559fab2a16480b4f7f1cbd8">>,
+                <<"28fddef86da4244accc0a4fe3b316f26">>],
+    Constants = [16#01, 16#02, 16#04, 16#08, 16#10,
+                 16#20, 16#40, 16#80, 16#1B, 16#36],
+    F = fun(In={Const, Expect}, Last) ->
+                io:format("~p~n", [In]),
+                LastBytes = cryptopals_bytes:hex_decode(Last),
+                Matrix = cryptopals_block:block_matrix(4, LastBytes),
+                [A, B, C, D] = cryptopals_block:aes_roundkey(Const, Matrix),
+                CurrentBytes = <<A/binary, B/binary, C/binary, D/binary>>,
+                CurrentHex = cryptopals_bytes:hex_encode(CurrentBytes),
+                ?assertEqual(cryptopals_bytes:new_hex(Expect), CurrentHex),
+                CurrentHex
+        end,
+    lists:foldl(F, cryptopals_bytes:hex_encode(?SIMPLE_KEY),
+                lists:zip(Constants, Expected)).
+
+apply_aes_test() ->
+    KeyMatrix0 = cryptopals_block:block_matrix(4, ?SIMPLE_KEY),
+    TextMatrix = cryptopals_block:block_matrix(4, ?SIMPLE_PLAINTEXT),
+    Expected0 = [<<16#00, 16#1F, 16#0E, 16#54>>,
+                 <<16#3C, 16#4E, 16#08, 16#59>>,
+                 <<16#6E, 16#22, 16#1B, 16#0B>>,
+                 <<16#47, 16#74, 16#31, 16#1A>>],
+    Round0 = cryptopals_block:apply_aes(KeyMatrix0, TextMatrix),
+    ?assertEqual(Expected0, Round0).
+
+apply_state_transform_test() ->
+    ok.
+
+matrix_binary([A, B, C, D]) ->
+    <<A/binary, B/binary, C/binary, D/binary>>.
